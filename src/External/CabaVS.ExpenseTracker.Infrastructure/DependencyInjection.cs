3abae5e260970b944fbc2b983ba5f342ed3ede7d
CabaVS <valerii.savchenko@icloud.com>
2025-04-05 17:27:17 +0200

    Remove unused Open Telemetry packages

--- src/External/CabaVS.ExpenseTracker.Infrastructure/DependencyInjection.cs
+++ src/External/CabaVS.ExpenseTracker.Infrastructure/DependencyInjection.cs
@@ -1,25 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 namespace CabaVS.ExpenseTracker.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-    {
-        services.AddOpenTelemetry()
-            .ConfigureResource(resource => resource.AddService("CabaVS.ExpenseTracker"))
-            .WithTracing(tracing =>
-            {
-                tracing
-                    .AddAspNetCoreInstrumentation()
-                    .AddHttpClientInstrumentation()
-                    .AddSqlClientInstrumentation(o => o.SetDbStatementForText = true);
-                tracing
-                    .AddOtlpExporter();
-            });
-        
-        return services;
-    }
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services) => services;
 }
